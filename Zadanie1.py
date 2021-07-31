215786ddca7f9e44bed671a42cc14f7d2c1f67a9
Paweł Staniucha <pawelstaniucha@wp.pl>
2021-07-31 13:48:08 +0200

    Ilość produktów w danym sklepie

--- Zadanie1.py
+++ Zadanie1.py
@@ -1,1 +1,11 @@
 print("Zadanie1")
+shopping_list = ["chleb","bułki","pączek","marchew","seler","rukola"]
+shopping_dict = {
+    "piekarnia": ["chleb","bułki","pączek"],
+    "warzywniak": ["marchew","seler","rukola"]
+}
+for shops, product in shopping_dict.items():
+    print(f"ide do {shops.capitalize()} po {[name.capitalize()for name in product]}")
+print (f"W sumie kupiłem {len(shopping_list)} produktów")
+for shops, product in shopping_dict.items():
+    print(f"W {shops} kupuje {len(product)} produktów")
--- Zadanie2.py
+++ Zadanie2.py
@@ -1,1 +1,10 @@
 print("Zadanie2")
+a = []
+cubed =[]
+
+for i in range(100):
+    if i % 5 == 0:
+        a.append(i)
+cubed = [number * number * number for  number in a]
+print(a)
+print(cubed)
