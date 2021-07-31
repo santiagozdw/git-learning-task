82c97b50781a2fb3850598dd42dc585df5017c5e
Paweł Staniucha <pawelstaniucha@wp.pl>
2021-07-31 13:58:57 +0200

    Liczby do kwadratu

--- Zadanie2.py
+++ Zadanie2.py
@@ -1,10 +1,12 @@
 print("Zadanie2")
 a = []
-cubed =[]
-
+cubed = []
+squared = []
 for i in range(100):
     if i % 5 == 0:
         a.append(i)
 cubed = [number * number * number for  number in a]
+squared = [number * number for  number in a]
 print(a)
+print(squared)
 print(cubed)
